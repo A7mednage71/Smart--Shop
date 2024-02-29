9fcbf77358a703ac86b2699bd34a46fe263225ac
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-01 00:27:45 +0200

    start

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShopApp());
+}
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
