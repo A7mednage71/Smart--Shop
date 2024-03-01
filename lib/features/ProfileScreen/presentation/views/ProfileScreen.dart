19c0c3ffd6d914b207e9a948e7ff152a0d91188c
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-02 01:54:50 +0200

    add profile photos and build part of profile ui

--- lib/features/ProfileScreen/presentation/views/ProfileScreen.dart
+++ lib/features/ProfileScreen/presentation/views/ProfileScreen.dart
@@ -9,10 +9,58 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profile Screen"),
-        leading: Image.asset(AppAssets.shoppingCart),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset(AppAssets.shoppingCart),
+        ),
       ),
-      body: const Center(
-        child: Text("ProfileScreen"),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, left: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Theme.of(context).cardColor,
+                      border: Border.all(width: 3, color: Colors.red.shade300),
+                      image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                            "https://cdn-icons-png.flaticon.com/512/219/219969.png",
+                          ))),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Ahmed Ben Nageh"),
+                    Text("Ahmed_Nageh@gmail.com"),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "General",
+              style: TextStyle(fontSize: 20),
+            ),
+            ListTile(
+              leading: Image.asset(AppAssets.address),
+            )
+          ],
+        ),
       ),
     );
   }
