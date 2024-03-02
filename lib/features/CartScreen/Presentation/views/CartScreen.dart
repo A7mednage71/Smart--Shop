7823d590aeafb360274a1b42d34d24a4332c67be
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-03 01:07:13 +0200

    create appbar for cartscreen

--- lib/features/CartScreen/Presentation/views/CartScreen.dart
+++ lib/features/CartScreen/Presentation/views/CartScreen.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:smartshop/core/assets_manger/assets_manager.dart';
 import 'package:smartshop/core/widgets/customemptydatawidget.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
-
+  final bool isempty = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CustomEmptyDataWidget(
-        title: 'Your Cart is empty',
-        subtitle:
-            "Looks like you have not added anything to your cart . Go ahead & explore a top categories",
-        buttomtext: 'Shop now',
-      ),
-    );
+    return isempty
+        ? const Scaffold(
+            body: CustomEmptyDataWidget(
+              title: 'Your Cart is empty',
+              subtitle:
+                  "Looks like you have not added anything to your cart . Go ahead & explore a top categories",
+              buttomtext: 'Shop now',
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text("Shopping basket"),
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Image.asset(AppAssets.shoppingCart),
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.delete_forever_rounded)),
+              ],
+            ),
+            body: const Column(
+              children: [],
+            ),
+          );
   }
 }
