bfed1649ad1e4b11b508f55bb98b88d0864c9996
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-03 22:39:58 +0200

    create list view of cart item

--- lib/features/CartScreen/Presentation/views/CartScreen.dart
+++ lib/features/CartScreen/Presentation/views/CartScreen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:smartshop/core/assets_manger/assets_manager.dart';
 import 'package:smartshop/core/widgets/customemptydatawidget.dart';
-import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:smartshop/features/CartScreen/Presentation/views/widgets/cartitem.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -32,76 +31,13 @@ class CartScreen extends StatelessWidget {
               ],
             ),
             body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 150,
-                        width: 130,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(16),
-                          child: FancyShimmerImage(
-                            imageUrl:
-                                "https://www.shutterstock.com/image-photo/white-sneakers-on-background-including-260nw-1100736923.jpg",
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Expanded(
-                                  child: Text(
-                                    "AdidasAdidasAdidasAdidasAdidas Ultra- 21",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                  ),
-                                ),
-                                Column(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.delete_outlined),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(IconlyLight.heart),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  "\$1085.00",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w400),
-                                ),
-                                ElevatedButton.icon(
-                                    onPressed: () {},
-                                    icon: const Icon(IconlyLight.arrowDown2),
-                                    label: const Text("Qty:1"))
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return const CartItem();
+                  },
+                )),
           );
   }
 }
---
+++ lib/features/CartScreen/Presentation/views/widgets/cartitem.dart
@@ -0,0 +1,77 @@
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 150,
+            width: 130,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: FancyShimmerImage(
+                imageUrl:
+                    "https://www.shutterstock.com/image-photo/white-sneakers-on-background-including-260nw-1100736923.jpg",
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        "AdidasAdidasAdidasAdidasAdidas Ultra- 21",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.delete_outlined),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(IconlyLight.heart),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "\$1085.00",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                    ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(IconlyLight.arrowDown2),
+                        label: const Text("Qty:1"))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
