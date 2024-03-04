d66e3a8dcb749339e11ae5bdd9ad1f6bc3fa2cae
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-05 00:43:48 +0200

    add swiper widget

--- lib/features/homeView/presentation/views/homeview.dart
+++ lib/features/homeView/presentation/views/homeview.dart
@@ -1,27 +1,53 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:smartshop/core/utils/themedata/provider/themeData_provider.dart';
+import 'package:smartshop/core/assets_manger/assets_manager.dart';
+import 'package:smartshop/core/widgets/App_Name_Shimmer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeDataProvider>(context);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Ahmd Nageh"),
-          SwitchListTile(
-            value: themeProvider.isDarkTheme(),
-            title:
-                Text(themeProvider.isDarkTheme() ? "dark mode" : "Light mode"),
-            onChanged: (value) {
-              themeProvider.settheme(themevalue: value);
-            },
-          )
-        ],
+      appBar: AppBar(
+        title: const AppNameWidget(name: "Shop Smart"),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Image.asset(AppAssets.shoppingCart),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.asset(
+                      AppAssets.panners[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  itemCount: AppAssets.panners.length,
+                  autoplay: true,
+                  pagination: const SwiperPagination(
+                    alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                      activeColor: Colors.red,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
