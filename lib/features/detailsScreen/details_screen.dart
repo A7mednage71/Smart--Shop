267e4002ea4892df718f6c421c1ed17919d2f8f0
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-05 11:48:25 +0200

    create routname for class

---
+++ lib/features/detailsScreen/details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  const ProductDetailsScreen({super.key});
+  static const routname = "DetailsScreen";
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
