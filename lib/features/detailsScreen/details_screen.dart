b7e5a2e068839afa182185b12398d50fecdedd86
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-05 12:18:23 +0200

    add appbar to details screen

--- lib/features/detailsScreen/details_screen.dart
+++ lib/features/detailsScreen/details_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:smartshop/core/widgets/App_Name_Shimmer.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   const ProductDetailsScreen({super.key});
   static const routname = "DetailsScreen";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const AppNameWidget(name: "Product detail"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+            size: 22,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+    );
   }
 }
