a46bdf64b38e88dd1aa9af330eebadc4ae8a6c95
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-05 16:29:12 +0200

    create wishlist screen

--- lib/features/wishlistScreen/wishlist_screen.dart
+++ lib/features/wishlistScreen/wishlist_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:smartshop/core/assets_manger/assets_manager.dart';
+import 'package:smartshop/core/widgets/customemptydatawidget.dart';
 
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({super.key});
   static const routname = "WishlistScreen";
+  final bool isempty = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: CustomEmptyDataWidget(
+        title: 'Your wishlist is empty',
+        subtitle: "seems like you don't have any wishes here",
+        buttomtext: 'Make a wish now',
+        image: AppAssets.bagWish,
+      ),
+    );
   }
 }
