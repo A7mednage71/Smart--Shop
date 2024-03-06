8ccb8595266c9324164caa4a9e9a2187706fef19
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-07 01:30:00 +0200

    build register screen ui without image

--- lib/features/registerscreen/presentation/views/register_screen.dart
+++ lib/features/registerscreen/presentation/views/register_screen.dart
@@ -1,11 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:smartshop/core/widgets/App_Name_Shimmer.dart';
+import 'package:smartshop/core/widgets/sighButton.dart';
+import 'package:smartshop/features/loginScreen/presentation/views/widgets/default_text_form_field.dart';
+import 'package:smartshop/features/loginScreen/presentation/views/widgets/password_text_form_field.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   static const routname = 'RegisterScreen';
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  late TextEditingController _emailtextfieldcontroller;
+  late TextEditingController _usertextfieldcontroller;
+  late TextEditingController _passwordtextfieldcontroller;
+  late TextEditingController _Repeatpasswordtextfieldcontroller;
+
+  late FocusNode _userfocusnode;
+  late FocusNode _emailfocusnode;
+  late FocusNode _passwordfocusnode;
+  late FocusNode _Repeatpasswordfocusnode;
+
+  GlobalKey<FormState> key = GlobalKey();
+
+  final AutovalidateMode _autovalidatemode = AutovalidateMode.always;
+
+  @override
+  void initState() {
+    _usertextfieldcontroller = TextEditingController();
+    _emailtextfieldcontroller = TextEditingController();
+    _passwordtextfieldcontroller = TextEditingController();
+    _Repeatpasswordtextfieldcontroller = TextEditingController();
+
+    _userfocusnode = FocusNode();
+    _emailfocusnode = FocusNode();
+    _passwordfocusnode = FocusNode();
+    _Repeatpasswordfocusnode = FocusNode();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 22,
+            ),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: key,
+            autovalidateMode: _autovalidatemode,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const AppNameWidget(
+                    name: "Welcome to shop smart",
+                    size: 30,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Welcome",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                  const Text(
+                      "Sign up now to receive special offers and updates from our app"),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  DefaultCustomFormField(
+                    textfieldcontroller: _usertextfieldcontroller,
+                    focusnode: _userfocusnode,
+                    focusnodeto: _emailfocusnode,
+                    inputaction: TextInputAction.next,
+                    inputtype: TextInputType.name,
+                    hint: "User123",
+                    icon: IconlyLight.addUser,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  DefaultCustomFormField(
+                    textfieldcontroller: _emailtextfieldcontroller,
+                    focusnode: _emailfocusnode,
+                    focusnodeto: _passwordfocusnode,
+                    inputaction: TextInputAction.next,
+                    inputtype: TextInputType.emailAddress,
+                    hint: "youremail@gmail.com",
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  PasswordCustomFormField(
+                    passwordtextfieldcontroller: _passwordtextfieldcontroller,
+                    passwordfocusnode: _passwordfocusnode,
+                    focusto: _Repeatpasswordfocusnode,
+                    passwordinputaction: TextInputAction.next,
+                    passwordinputtype: TextInputType.visiblePassword,
+                    hint: 'Password',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  PasswordCustomFormField(
+                    passwordtextfieldcontroller:
+                        _Repeatpasswordtextfieldcontroller,
+                    passwordfocusnode: _Repeatpasswordfocusnode,
+                    focusto: _Repeatpasswordfocusnode,
+                    passwordinputaction: TextInputAction.done,
+                    passwordinputtype: TextInputType.visiblePassword,
+                    hint: 'Repeat Password',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SignButton(
+                    text: "Sign Up",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
