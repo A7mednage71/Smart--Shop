3058ca02efba9a6f5698a1f27fb87932fb83dd16
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-08 18:22:04 +0200

    create product model

---
+++ lib/core/models/product_model.dart
@@ -0,0 +1,19 @@
+class ProductModel {
+  final String productID;
+  final String productName;
+  final String productPrice;
+  final String productCategory;
+  final String productDescription;
+  final String productImage;
+  final String productQuantity;
+
+  ProductModel({
+    required this.productID,
+    required this.productName,
+    required this.productPrice,
+    required this.productCategory,
+    required this.productDescription,
+    required this.productImage,
+    required this.productQuantity,
+  });
+}
