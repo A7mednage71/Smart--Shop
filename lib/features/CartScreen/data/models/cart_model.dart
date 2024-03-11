f3d45f945f4260f446f7fda3753bd8a6e47bbc52
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-12 00:01:06 +0200

    create card model

---
+++ lib/features/CartScreen/data/models/cart_model.dart
@@ -0,0 +1,11 @@
+class CartModel {
+  final String cartId;
+  final String productId;
+  final String quantity;
+
+  CartModel({
+    required this.cartId,
+    required this.productId,
+    required this.quantity,
+  });
+}
